bcb534503c2d17a612614aa416c0628de270449f
Wirkmood <wirkmood@gmail.com>
2017-10-06 13:32:15 +0200

    Add files via upload

--- install_docker.sh
+++ install_docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -v
 
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo echo "apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D" >> /etc/sources.l$
+sudo echo "apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D" >> /etc/sources.list
 sudo apt-get update -y
 sudo apt-get install docker-engine docker git
-
-sudo git clone https://github.com/wirkmood/Ansible_script /home/
+sudo mkdir /home/docker/
+sudo git clone https://github.com/wirkmood/Docker_repository /home/docker
